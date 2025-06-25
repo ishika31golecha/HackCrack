4a2967a0a0150f055bc2dd9a4f3187169c374385
ishika31golecha <169146987+ishika31golecha@users.noreply.github.com>
2025-06-25 18:57:21 +0530

    Update index.js

--- HackCrack/server/index.js
+++ HackCrack/server/index.js
@@ -8,7 +8,7 @@
 // // Middleware
 // app.use(express.json());
 // app.use(cors({
-//   origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173'],
+//   origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173', 'https://hack-crack.vercel.app/'],
 //   credentials: true
 // }));
 
